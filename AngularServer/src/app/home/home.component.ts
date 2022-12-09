be398287e301ba545846ec1b732677fb3f1ed88f
PortaMatteo <porta.matteo@iisgalvanimi.edu.it>
2022-12-09 17:50:31 +0000

    .

--- AngularServer/src/app/home/home.component.ts
+++ AngularServer/src/app/home/home.component.ts
@@ -10,7 +10,7 @@ import { HttpClient } from '@angular/common/http';
 export class HomeComponent {
   search!: any;
   loading!: Boolean;
-  url: string = "https://3245-portamatteo-progettosql-7mw9jyajuwk.ws-eu77.gitpod.io/home";
+  url: string = "https://3245-portamatteo-progettosql-t4uwqe5s3xt.ws-eu78.gitpod.io/home";
 
   constructor(public http: HttpClient) {
     this.get(this.url);
